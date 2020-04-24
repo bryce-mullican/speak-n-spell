426dc96b28ebb392c0707122d95d01faebc04898
Bryce Mullican <bryce.mullican@gmail.com>
2020-04-24 12:05:31 -0500

    added in score and help feature

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import "react-simple-keyboard/build/css/index.css";
 import "./App.css";
 
 function App() {
-  const words = ["dad", "mom", "cat", "dog"];
-  const [input, setInput] = useState("Type what you hear");
+  const words = ["dad", "mom", "four", "hello"];
+  const [input, setInput] = useState("");
   const [word, setWord] = useState("");
   const [speech, setSpeech] = useState("");
+  const [letters, setLetters] = useState("");
+  const [display, setDisplay] = useState("");
+  const [score, setScore] = useState(0)
   const keyboard = useRef();
+  const [help, setHelp] = useState(false)
 
   const getVoice = selected => {
     const voices = window.speechSynthesis.getVoices();
@@ -17,7 +21,11 @@ function App() {
   };
 
   useEffect(() => {
-    setWord(words[Math.floor(Math.random() * words.length)]);
+    const word = words[Math.floor(Math.random() * words.length)];
+    const letterSpans = word.toUpperCase().split('').map((letter,idx) => <span key={idx}>{letter}</span>);
+    setLetters(letterSpans);
+    setDisplay();
+    setWord(word);
   }, []);
 
   // const onend = func => {
@@ -47,6 +55,11 @@ function App() {
 
   const newWord = () => {
     const word = words[Math.floor(Math.random() * words.length)];
+    const letterSpans = word.toUpperCase().split('').map((letter,idx) => <span key={idx}>{letter}</span>);
+    const wordSpans = word.toUpperCase().split('').map((letter,idx) => <span key={idx}></span>);
+    setHelp(false);
+    setLetters(letterSpans);
+    setDisplay(wordSpans);    
     setWord(word);
     setInput("");
     keyboard.current.clearInput();
@@ -54,47 +67,61 @@ function App() {
   };
 
   const onChange = input => {
+    const letterSpans = input.split('').map((letter,idx) => <span key={idx}>{letter}</span>);
+    setDisplay(letterSpans);
     setInput(input);
     setSpeech(input.toLowerCase());
-  };
-
-  const onKeyPress = button => {
-    if (button === "{enter}") {
-      speak(speech);
-
-      if (input.toLowerCase() === word) {
-        newWord();
-      }
-    }
   };
 
   const playWord = () => {
     speak(word);
   };
+  
+  const onKeyPress = button => {
+    if (button === "{enter}") {
+      speak(speech);
+
+      if (input.toLowerCase() === word) {
+        setScore(score + 1);
+        newWord();
+      }
+    }
+    if (button === "{shift}") {
+      playWord();
+    }
+  };
+
+  const showHelp = () => {
+    setHelp(!help);
+  }
 
   return (
     <div className="App">
-      <div className="input">{input}</div>
+      <div className="header">Score: {score} <button className="help" onClick={showHelp}>Help</button></div>
+      <div className="input">
+        <div className={help ? 'letters help' : 'letters'}><div className="letter-wrap">{letters}</div></div>
+        <div className="word">
+          <div className="letter-wrap" style={{width: (word.length * 170) + (word.length * 10)}}>{display}</div></div>
+      </div>
       <Keyboard
         keyboardRef={r => (keyboard.current = r)}
         onChange={onChange}
         onKeyPress={onKeyPress}
         display={{
           "{bksp}": "Delete Letter",
-          "{enter}": "Check"
+          "{enter}": "Check",
+          "{shift}": "Play Word"
         }}
         layout={{
           default: [
             "Q W E R T Y U I O P",
             "A S D F G H J K L",
             "Z X C V B N M",
-            "{bksp} {enter}"
+            "{bksp} {enter}",
+            "{shift}"
           ]
         }}
       />
-      <div className="word" onClick={playWord}>
-        Play Word
-      </div>
     </div>
   );
 }
